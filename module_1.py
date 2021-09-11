dbf81c2658110ef25c08e6d24c2989ba93358873
vv31415926 <valery31415926@mail.ru>
2021-09-11 19:08:59 +0500

    commit  main

---
+++ module_1.py
@@ -0,0 +1,5 @@
+def fun1_1:
+    print('module1 func 1')
+
+def fun1_2:
+    print('module1 func 2')
---
+++ module_4.py
@@ -0,0 +1,6 @@
+def fun4_1:
+    print('module4 func 1')
+
+
+def fun4_2:
+    print('module4 func 2')
